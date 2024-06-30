6b4daf7b2fa569a7cb13e6b8cfb560e9c7424a73
Eadweard <aurora.coder.zqf@gmail.com>
2024-07-01 00:09:28 +0800

    flask

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def home(): 
-    return 'Hello, World!'
+    return 'Hello, Flask!'
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
