6fcfe253ceb51656671dc6fffe47ba2a051c09d5
Eadweard <aurora.coder.zqf@gmail.com>
2024-07-01 09:34:03 +0800

    fix

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import flask
+from flask import request
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def home(): 
     return 'Hello, Flask!'
+
+
+@app.route('/index')
+def index():
+    index = request.args.get('index')
+    return index
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
